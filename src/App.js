3c26a6f4491e745ba99969ddb93ae5ddb83d31c5
jameswu <jamesw0462@gmail.com>
2023-04-23 00:33:35 -0700

    Re-implementing textrank

--- src/App.js
+++ src/App.js
@@ -1,54 +1,7 @@
 import React, { useState } from 'react';
 import './App.css';
 import shorten from './summarization';
-import * as DOMPurify from "dompurify";
-import html2md from "html-to-md";
 
-function getContainer() {
-  // let containers = document.getElementsByTagName('article');
-  // console.log(containers);
-  // let maxTextLength = -1;
-  // let maxTextContainer = null;
-
-  // for (let i = 0; i < containers.length; i++) {
-  //   let textLength = containers[i].innerText.length;
-  //   if (textLength > maxTextLength) {
-  //     maxTextLength = textLength;
-  //     maxTextContainer = containers[i];
-  //   }
-  // }
-
-  // return maxTextContainer;
-  return document.body;
-}
-
-
-function getContentOfArticle() {
-  // let pageSelectedContainer = getContainer();
-  let pageSelectedContainer = document.body;
-  let content = pageSelectedContainer.textContent.trim();
-  // console.log(content);
-  return content;
-}
-
-
-// function getContentOfArticle() {
-//   let pageSelectedContainer = getContainer();
-
-//   const pattern1 = /<a\b[^>]*>(.*?)<\/a>/gi;
-//   pageSelectedContainer.innerHTML = DOMPurify.sanitize(
-//     pageSelectedContainer.innerHTML.replace(pattern1, "")
-//   );
-//   const pattern2 = new RegExp("<br/?>[ \r\ns]*<br/?>", "g");
-//   pageSelectedContainer.innerHTML = DOMPurify.sanitize(
-//     pageSelectedContainer.innerHTML.replace(pattern2, "</p><p>")
-//   );
-
-//   let content = DOMPurify.sanitize(pageSelectedContainer.innerHTML);
-//   console.log(content);
-//   // content = html2md(content);
-//   return content;
-// }
 
 function App() {
   const [report, setReport] = useState('');
@@ -62,14 +15,12 @@ function App() {
           target: { tabId: activeTabId },
           function: () => {
             // return document.body.innerText; 
-            // const headingsAndParagraphs = document.querySelectorAll('p');
-            // let text = '';
-            // for (let i = 0; i < headingsAndParagraphs.length; i++) {
-            //   text += headingsAndParagraphs[i].textContent + ' ';
-            // }
-            // return text;
-            let content = getContentOfArticle();
-            return content;
+            const headingsAndParagraphs = document.querySelectorAll('p');
+            let text = '';
+            for (let i = 0; i < headingsAndParagraphs.length; i++) {
+              text += headingsAndParagraphs[i].textContent + ' ';
+            }
+            return text;
           }
         },
         (results) => {
